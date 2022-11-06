0e060da0250fa00d2a4ce354d8b61d9dede32403
Bruno Garcia <bruno.garcia@wallbox.com>
2022-11-06 16:27:49 +0100

    feat: create the badge component & story

---
+++ src/components/Badge/Badge.stories.tsx
@@ -0,0 +1,19 @@
+// src/components/Badge/Badge.stories.tsx
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { Badge } from './Badge'
+
+// Metadata of our component
+export default {
+  title: 'Components/Badge',
+  component: Badge,
+} as ComponentMeta<typeof Badge>
+
+// Base Template
+const Template: ComponentStory<typeof Badge> = (args) => <Badge {...args} />
+
+// Story, a component variation consisting of a template + args
+export const Default = Template.bind({})
+Default.args = {
+  text: 'Comfort food',
+}
