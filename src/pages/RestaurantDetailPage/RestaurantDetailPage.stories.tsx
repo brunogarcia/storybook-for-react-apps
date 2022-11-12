44a46d296c6237b843ae709da356ed296ab541d4
Bruno Garcia <bruno.garcia@wallbox.com>
2022-11-12 15:43:04 +0100

    feat: adding the restaurant-detail-page story

--- src/pages/RestaurantDetailPage/RestaurantDetailPage.stories.tsx
+++ src/pages/RestaurantDetailPage/RestaurantDetailPage.stories.tsx
@@ -1,4 +1,8 @@
+import { rest } from 'msw'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { BASE_URL } from '../../api'
+import { restaurants } from '../../stub/restaurants'
 
 import { RestaurantDetailPage } from './RestaurantDetailPage'
 
@@ -26,5 +30,57 @@ Success.parameters = {
   design: {
     type: 'figma',
     url: 'https://www.figma.com/proto/3Q1HTCalD0lJnNvcMoEw1x/Mealdrop?node-id=169%3A510&scaling=scale-down-width&page-id=135%3A257&starting-point-node-id=135%3A258',
+  },
+  msw: {
+    handlers: [
+      rest.get(BASE_URL, (req, res, ctx) => {
+        return res(ctx.json(restaurants[0]))
+      }),
+    ],
+  },
+}
+
+export const Loading = Template.bind({})
+Loading.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/3Q1HTCalD0lJnNvcMoEw1x/Mealdrop?node-id=2152%3A3158',
+  },
+  msw: {
+    handlers: [
+      rest.get(BASE_URL, (req, res, ctx) => {
+        return res(ctx.delay('infinite'))
+      }),
+    ],
+  },
+}
+
+export const NotFound = Template.bind({})
+NotFound.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/3Q1HTCalD0lJnNvcMoEw1x/Mealdrop?node-id=1097%3A3785',
+  },
+  msw: {
+    handlers: [
+      rest.get(BASE_URL, (req, res, ctx) => {
+        return res(ctx.status(404))
+      }),
+    ],
+  },
+}
+
+export const Error = Template.bind({})
+Error.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/3Q1HTCalD0lJnNvcMoEw1x/Mealdrop?node-id=1091%3A4537',
+  },
+  msw: {
+    handlers: [
+      rest.get(BASE_URL, (req, res, ctx) => {
+        return res(ctx.status(500))
+      }),
+    ],
   },
 }
