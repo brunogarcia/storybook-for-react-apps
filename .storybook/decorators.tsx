25e671bb2e8a7909010e512abdf63adfd93f2de8
Bruno Garcia <bruno.garcia@wallbox.com>
2022-11-12 15:37:25 +0100

    feat: adding the restaurant-detail-page story

--- .storybook/decorators.tsx
+++ .storybook/decorators.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { DecoratorFn } from '@storybook/react'
-import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { withDesign } from 'storybook-addon-designs'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider as StoreProvider } from 'react-redux'
+import { BrowserRouter, MemoryRouter, Route, Routes } from 'react-router-dom'
 import { initialize as initializeMSW, mswDecorator } from 'msw-storybook-addon'
 
 import { rootReducer } from '../src/app-state'
@@ -16,6 +16,32 @@ initializeMSW()
 const ThemeProviderProxy: any = ThemeProvider
 const GlobalStyleProxy: any = GlobalStyle
 
+const withTheme: DecoratorFn = (StoryFn, { parameters, globals }) => {
+  const theme = parameters.theme || globals.theme
+  const storyTheme = theme === 'dark' ? darkTheme : lightTheme
+  return (
+    <ThemeProviderProxy theme={storyTheme}>
+      <GlobalStyleProxy />
+      <StoryFn />
+    </ThemeProviderProxy>
+  )
+}
+
+/**
+ *
+ * Provide components support for Redux store
+ * optionally passing custom initial state, and using default initial state if not passed
+ *
+ * @example
+ * export const MyComponent = () => Template.bind({})
+ * MyComponent.parameters = {
+ *   store: {
+ *     initialState: {
+ *       foo: 'bar'
+ *     },
+ *   }
+ * };
+ */
 export const withStore: DecoratorFn = (StoryFn, { parameters }) => {
   // Create a store by merging optional custom initialState coming from story parameters
   const store = configureStore({
@@ -30,21 +56,43 @@ export const withStore: DecoratorFn = (StoryFn, { parameters }) => {
   )
 }
 
-const withTheme: DecoratorFn = (StoryFn, { parameters, globals }) => {
-  const theme = parameters.theme || globals.theme
-  const storyTheme = theme === 'dark' ? darkTheme : lightTheme
+/**
+ *
+ * Provide components support for routing support and simulated deeplinking
+ * it renders the component with a mocked history based on the route passed
+ *
+ * @example`
+ * export const MyComponent = () => Template.bind({})
+ * MyComponent.parameters = {
+ *   deeplink: {
+ *     path = '/restaurant/:id',
+ *     route = '/restaurant/12',
+ *   }
+ * };
+ */
+export const withRouter: DecoratorFn = (StoryFn, { parameters: { deeplink } }) => {
+  // if there's no deeplink parameter, just return the story in a BrowserRouter
+  if (!deeplink) {
+    return (
+      <BrowserRouter>
+        <StoryFn />
+      </BrowserRouter>
+    )
+  }
+
+  // if there is a deeplink parameter, wrap the story with a simulated route in MemoryRouter
+  const { path, route } = deeplink
+
   return (
-    <ThemeProviderProxy theme={storyTheme}>
-      <GlobalStyleProxy />
-      <StoryFn />
-    </ThemeProviderProxy>
+    <MemoryRouter
+      // encode the route to simulate what the browser would do
+      initialEntries={[encodeURI(route)]}
+    >
+      <Routes>
+        <Route path={path} element={<StoryFn />} />
+      </Routes>
+    </MemoryRouter>
   )
 }
-
-export const withRouter: DecoratorFn = (StoryFn) => (
-  <BrowserRouter>
-    <StoryFn />
-  </BrowserRouter>
-)
 
 export const globalDecorator = [mswDecorator, withStore, withTheme, withDesign, withRouter]
---
+++ src/pages/RestaurantDetailPage/RestaurantDetailPage.stories.tsx
@@ -0,0 +1,30 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { RestaurantDetailPage } from './RestaurantDetailPage'
+
+export default {
+  title: 'Pages/RestaurantDetailPage',
+  component: RestaurantDetailPage,
+  parameters: {
+    layout: 'fullscreen',
+    deeplink: {
+      path: '/restaurants/:id',
+      route: '/restaurants/1',
+    },
+  },
+} as ComponentMeta<typeof RestaurantDetailPage>
+
+const Template: ComponentStory<typeof RestaurantDetailPage> = () => (
+  <>
+    <RestaurantDetailPage />
+    <div id="modal" />
+  </>
+)
+
+export const Success = Template.bind({})
+Success.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/proto/3Q1HTCalD0lJnNvcMoEw1x/Mealdrop?node-id=169%3A510&scaling=scale-down-width&page-id=135%3A257&starting-point-node-id=135%3A258',
+  },
+}
